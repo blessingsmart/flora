3aaf98973e2535d6bb85d80330d49c2342a19d09
Blessing Eno <bublessingsmart@gmail.com>
2024-04-18 22:28:44 +0100

    modified:   src/component/navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = ({ openModal }) => {
 
   return (
     <>
-      <div className="flex justify-between drop-shadow-md sm:px-20 px-5 items-center  h-32 text-white bg-black z-30">
+      <div className="flex justify-between drop-shadow-md sm:px-20 px-5 items-center h-24 sm:h-32 text-white bg-black z-30">
         <button onClick={() => openModal("nav")}>
           <IoReorderThreeOutline size={35} />
         </button>
-        <Link to="/">
-          <img src={logo} alt="logo " className='size-20'/>
+        <Link to="/" className='h-24 flex py-2'>
+          <img src={logo} alt="logo "/>
         </Link>
         <div className="flex gap-4">
           <button onClick={() => openModal("search")}>
